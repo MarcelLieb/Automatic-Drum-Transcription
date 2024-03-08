60993952320ba92ea58a0368780be915f0216ee7
Marclie <marclie1@gmx.de>
2024-03-08 18:12:35 +0100

    thresholding

--- SpecFlux.py
+++ SpecFlux.py
@@ -2,6 +2,7 @@ import librosa.filters
 import torch
 import torch.nn as nn
 import torchaudio
+import torch.nn.utils.parametrize as P
 
 
 class SpecFlux(nn.Module):
@@ -30,7 +31,7 @@ class SpecFlux(nn.Module):
         self.lamb = lamb
         self.n_mels = n_mels
         self.filter_bank = torchaudio.transforms.MelScale(
-            n_mels=n_mels, sample_rate=sample_rate, n_stft=n_fft // 2 + 1, f_min=0.0, f_max=20000
+            n_mels=n_mels, sample_rate=sample_rate, n_stft=n_fft // 2 + 1, f_min=0.0, f_max=20000, norm=None, mel_scale="htk",
         )
         self.window = torch.tensor(
             librosa.filters.get_window(window="hann", fftbins=False, Nx=win_length),
@@ -39,6 +40,10 @@ class SpecFlux(nn.Module):
             device=device,
         )
         self.feature_extractor = nn.Conv1d(in_channels=n_mels, out_channels=3, kernel_size=1, padding=0, bias=False)
+        self.drum_threshold = Threshold(mean_range=6, max_range=3, norm_range=8)
+        self.snare_threshold = Threshold(mean_range=6, max_range=3, norm_range=8)
+        self.hihat_threshold = Threshold(mean_range=6, max_range=3, norm_range=8)
+        self.onset_threshold = Threshold(mean_range=6, max_range=3, norm_range=8)
 
     def forward(self, x):
         spec = torch.stft(
@@ -62,7 +67,69 @@ class SpecFlux(nn.Module):
         # self.feature_extractor = nn.Conv1d(n_mels, 3, 1, padding=0, bias=False)
         features = self.feature_extractor(spec_diff)
 
-        return torch.cat((drum_spec, snare_spec, hihat_spec), dim=-2)
+        drum_spec = self.drum_threshold(features[:, 0, :].unsqueeze(-2))
+        hihat_spec = self.hihat_threshold(features[:, 1, :].unsqueeze(-2))
+        snare_spec = self.snare_threshold(features[:, 2, :].unsqueeze(-2))
+
+        spec_flux = torch.sum(features, dim=-2, keepdim=True)
+        spec_flux = self.onset_threshold(spec_flux)
+
+        return torch.cat((drum_spec, hihat_spec, snare_spec, spec_flux), dim=-2)
+
+
+class Threshold(nn.Module):
+    def __init__(self, channels=1, mean_range=1, max_range=1, norm_range=1, **kwargs):
+        super(Threshold, self).__init__()
+        self.threshold = nn.Conv1d(channels, channels, 1, padding=0, bias=True)
+        P.register_parametrization(self.threshold, "weight", torch.nn.ReLU())
+        P.register_parametrization(self.threshold, "bias", torch.nn.ReLU())
+        self.max = CausalMaxPool1d(kernel_size=max_range, **kwargs)
+        self.mean = CausalAvgPool1d(kernel_size=mean_range, **kwargs)
+        self.norm = CausalAvgPool1d(kernel_size=norm_range, **kwargs)
+
+    def forward(self, x):
+        mean = self.mean(x)
+        maximums = self.max(x)
+        norm = self.norm(x)
+        threshold = self.threshold(norm) + mean
+        mask = (x >= maximums) & (x >= threshold)
+        x[~mask] = 0
+        x[mask] = 1
+        return x
+
+
+class CausalConv1d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, dilation=1, **kwargs):
+        super(CausalConv1d, self).__init__()
+        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size, padding=(kernel_size - 1) * dilation, dilation=dilation, **kwargs)
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.causal_conv.padding[0] != 0:
+            x = x[..., :-self.causal_conv.padding[0]]
+        return x
+
+class CausalMaxPool1d(nn.Module):
+    def __init__(self, kernel_size, stride=1, dilation=1, **kwargs):
+        super(CausalMaxPool1d, self).__init__()
+        self.padding = ((kernel_size - 1) * dilation, 0)
+        self.max_pool = nn.MaxPool1d(kernel_size, stride, padding=0, dilation=dilation, **kwargs)
+
+    def forward(self, x):
+        x = nn.functional.pad(x, self.padding)
+        x = self.max_pool(x)
+        return x
+
+class CausalAvgPool1d(nn.Module):
+    def __init__(self, kernel_size, stride=1, dilation=1, **kwargs):
+        super(CausalAvgPool1d, self).__init__()
+        self.padding = ((kernel_size - 1) * dilation, 0)
+        self.avg_pool = nn.AvgPool1d(kernel_size, stride, padding=0, **kwargs)
+
+    def forward(self, x):
+        x = nn.functional.pad(x, self.padding)
+        x = self.avg_pool(x)
+        return x
 
 
 if __name__ == '__main__':
