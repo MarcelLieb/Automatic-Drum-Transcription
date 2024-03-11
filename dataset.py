972debe95b75790811317a8351952986a7717a66
Marclie <marclie1@gmx.de>
2024-03-11 21:16:45 +0100

    spread out labels

--- dataset.py
+++ dataset.py
@@ -55,6 +55,7 @@ class RBMA_13(Dataset):
 
         annotations = load_rbma()
         self.annotations = {}
+        self.label_spreader = torch.nn.MaxPool1d(3, stride=1, padding=1)
         split = split.lower()
         assert split in ["train", "train_big", "validation", "test", "all"]
         if split == "all":
@@ -100,14 +101,15 @@ class RBMA_13(Dataset):
         annotation[:, 0] += self.label_shift
 
         frames = (audio.shape[0] - self.fft_size) // self.hop_size + 1
-        labels = np.zeros((4, frames), dtype=np.int64)
+        labels = torch.zeros((4, frames), dtype=torch.int64)
         indices = (annotation[:, 0] * self.sample_rate) // self.hop_size
-        indices = indices.astype(np.int64)
+        indices = torch.tensor(indices).long()
         for i in range(3):
             inst_indices = indices[annotation[:, 1] == i]
             labels[i, inst_indices] = 1
             labels[3, inst_indices] = 1
-        labels = torch.tensor(labels, dtype=torch.float32)
+        labels = labels.float()
+        labels = self.label_spreader(labels.unsqueeze(0)).squeeze(0)
         labels = labels.permute(1, 0)
 
         return audio, labels
