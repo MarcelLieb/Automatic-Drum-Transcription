9869640b28ceca52dae4461510053f14d54c0b14
Marclie <marclie1@gmx.de>
2024-04-08 23:47:52 +0200

    causal convolution

--- model/cnn.py
+++ model/cnn.py
@@ -1,25 +1,33 @@
-import torchaudio
 from torch import nn
 from torch.nn import functional as F
+from model.model import CausalConv2d
 
 
 class CNN(nn.Module):
     def __init__(self, n_mels=82, n_classes=3):
         super(CNN, self).__init__()
         self.n_mels = n_mels
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=7, padding=3, bias=False)
-        self.conv2 = nn.Conv2d(16, 16, kernel_size=3, padding=1, bias=False)
-        self.fc1 = nn.Linear(16 * n_mels, 256)
+        self.conv1 = CausalConv2d(1, 16, kernel_size=7, bias=False)
+        self.norm1 = nn.BatchNorm2d(16)
+        self.conv2 = CausalConv2d(16, 32, kernel_size=3, bias=False, dilation=1)
+        self.norm2 = nn.BatchNorm2d(32)
+        self.fc1 = nn.Linear(32 * n_mels, 256)
+        self.dropout = nn.Dropout(0.2)
         self.fc2 = nn.Linear(256, n_classes)
 
     def forward(self, x):
         batch_size = x.size(0)
         x = x.unsqueeze(1)
-        x = F.elu(self.conv1(x))
-        x = F.elu(self.conv2(x))
-        x = x.reshape((batch_size, 16 * self.n_mels, -1))
+        x = self.conv1(x)
+        x = self.norm1(x)
+        x = F.selu(x)
+        x = self.conv2(x)
+        # x = self.norm2(x)
+        x = F.selu(x)
+        x = x.reshape((batch_size, 32 * self.n_mels, -1))
         x = x.permute(0, 2, 1)
-        x = F.relu(self.fc1(x))
+        x = F.selu(self.fc1(x))
+        x = self.dropout(x)
         x = self.fc2(x)
         x = F.tanh(x)
         x = x.permute(0, 2, 1)
