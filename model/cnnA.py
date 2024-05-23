fix flux calculation

model/cnnA.py
@@ -42,8 +42,8 @@ class CNNAttention(nn.Module):
 
     def forward(self, x):
         if self.flux:
-            diff = x[1:] - x[-1]
-            diff = f.relu(f.pad(diff, (0, 0, 0, 0, 1, 0), mode="constant", value=0))
+            diff = x[..., 1:] - x[..., :-1]
+            diff = f.relu(f.pad(diff, (1, 0), mode="constant", value=0))
             x = torch.hstack((x, diff))
         x = x.unsqueeze(1)
         x = self.conv1(x)