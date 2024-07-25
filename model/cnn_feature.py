5b1b9bb68e9b58dd769bf7c577a274343e88ae12
Marclie <marclie1@gmx.de>
2024-07-25 15:43:24 +0200

    configurable layers

--- model/cnn_feature.py
+++ model/cnn_feature.py
@@ -5,47 +5,75 @@ from model import ResidualBlock
 
 
 class CNNFeature(nn.Module):
-    def __init__(self, num_channels, down_sample_factor, activation, causal):
+    def __init__(self, num_channels, n_layers, down_sample_factor, channel_multiplication, activation, causal):
         super(CNNFeature, self).__init__()
 
         self.activation = activation
         self.down_sample_factor = down_sample_factor
 
+        self.conv_pools = nn.ModuleList()
+        for i in range(n_layers):
+            if i == 0:
+                self.conv_pools.append(
+                    nn.Sequential(
+                        ResidualBlock(
+                            1,
+                            num_channels * (channel_multiplication ** i),
+                            kernel_size=3,
+                            causal=causal,
+                            activation=self.activation,
+                        ),
+                        nn.MaxPool2d(
+                            kernel_size=(down_sample_factor, 1),
+                            stride=(down_sample_factor, 1),
+                        ),
+                    )
+                )
+                continue
 
-        self.conv1 = ResidualBlock(
-            1, num_channels, kernel_size=3, causal=causal, activation=self.activation
-        )
+            self.conv_pools.append(
+                nn.Sequential(
+                    ResidualBlock(
+                        num_channels * (channel_multiplication ** (i - 1)),
+                        num_channels * (channel_multiplication ** i),
+                        kernel_size=3,
+                        causal=causal,
+                        activation=self.activation,
+                    ),
+                    nn.MaxPool2d(
+                        kernel_size=(down_sample_factor, 1),
+                        stride=(down_sample_factor, 1),
+                    ),
+                )
+            )
 
-        self.pool1 = nn.MaxPool2d(
-            kernel_size=(down_sample_factor, 1), stride=(down_sample_factor, 1)
-        )
-
-        self.conv2 = ResidualBlock(
-            num_channels,
-            num_channels * 2,
-            kernel_size=3,
-            causal=causal,
-            activation=self.activation,
-        )
-
-        self.pool2 = nn.MaxPool2d(
-            kernel_size=(down_sample_factor, 1), stride=(down_sample_factor, 1)
-        )
-
-        self.resample = nn.Conv2d(
-            num_channels, num_channels * 2, kernel_size=1, stride=1
-        )
+        self.resamples = nn.ModuleList()
+        for i in range(n_layers):
+            if i == 0:
+                self.resamples.append(
+                    nn.Conv2d(
+                        1,
+                        num_channels * (channel_multiplication ** i),
+                        kernel_size=1,
+                        stride=1,
+                    )
+                )
+                continue
+            self.resamples.append(
+                nn.Conv2d(
+                    num_channels * (channel_multiplication ** (i - 1)),
+                    num_channels * (channel_multiplication ** i),
+                    kernel_size=1,
+                    stride=1,
+                )
+            )
 
     def forward(self, x):
         x = x.unsqueeze(1)
-        x = self.conv1(x)
-        x = self.pool1(x)
-        skip = x
-        x = self.conv2(x)
-        x = self.pool2(x)
-        x = x + self.resample(
-            torch.nn.functional.interpolate(skip, size=x.shape[2:], mode="nearest")
-        )
+        for conv_pool, resample in zip(self.conv_pools, self.resamples):
+            skip = x
+            x = conv_pool(x)
+            x = x + resample(torch.nn.functional.interpolate(skip, size=x.shape[2:], mode="nearest"))
 
         return x
 
